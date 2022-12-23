f634b07229e436990ad2a963a2efcb206125fbe1
Strega's Gate <55062873+STREGAsGate@users.noreply.github.com>
2022-12-23 15:55:35 -0500

    progress

---
+++ Xcode Syntax Highlighting/GravityUTI/GravityUTI/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  GravityUTI
+//
+//  Created by Dustin Collins on 12/23/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        exit(0)
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
